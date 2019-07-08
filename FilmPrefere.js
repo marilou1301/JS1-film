9cadad4947931651f69ff18ccf6c8becad134fa1
marilou1301 <marilou.prevoteau@gmail.com>
2019-07-08 17:03:43 +0200

    JS 1

---
+++ FilmPrefere.js
@@ -0,0 +1,10 @@
+﻿/* FilmPrefere.js */
+
+// Define some variables
+const myFilm = "L'été meurtrier";
+const filmDirector = 'Jean Becker';
+let releaseYear= 1983
+const message = 'Un de mes films préféré est ' + myFilm + ' de ' + filmDirector + ', il est sortie en ' + releaseYear + '.';
+
+// Display a popup
+alert(message);
